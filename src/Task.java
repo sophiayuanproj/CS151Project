2e5b932233a442fc9e4be746f309dbba1937763a
Sophia Yuan <sophia38yuan@gmail.com>
2020-10-24 00:57:56 -0700

    Created the task constructor, still need to work on details

--- src/Task.java
+++ src/Task.java
@@ -1,2 +1,19 @@
 public class Task {
+    private String theName;
+    private String theDescription;
+    private String theType;
+    private int startDate;
+    private int endDate;
+    private boolean theStatus;
+
+    public Task(String name, String description, String type, int start, int end, boolean status){
+        theName = name;
+        theDescription = description;
+        theType = type;
+        startDate = start;
+        endDate = end;
+        theStatus = status;
+    }
+
+
 }
--- src/Workout.java
+++ src/Workout.java
@@ -1,2 +1,7 @@
 public class Workout {
+
+    public Workout()
+    {
+
+    }
 }
