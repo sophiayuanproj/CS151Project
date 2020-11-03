2aa4a097ba8478565859b7575494d9fbaf3ceaf5
Sophia Yuan <sophia38yuan@gmail.com>
2020-11-03 01:54:27 -0800

    Filled in basic functions for task, may change in the future. Added javadoc to both TaskInterface and Task.

--- src/Task.java
+++ src/Task.java
@@ -1,11 +1,26 @@
+/**
+ * Author @Team 7
+ * Task class that models a task
+ */
+
 public class Task implements TaskInterface {
     private String theName;
     private String theDescription;
     private String thePriority;
-    private int startDate;
-    private int endDate;
+    private double startDate;
+    private double endDate;
     private boolean theStatus;
-    //sophia yuan
+
+    /**
+     * Task object is created here
+     *
+     * @param name        is the name of the task
+     * @param description is the description of the task
+     * @param priority    is the priority of the task
+     * @param start       is the start date for the task
+     * @param end         is the end date for the task
+     * @param status      is the status of the task
+     */
     public Task(String name, String description, String priority, int start, int end, boolean status) {
         theName = name;
         theDescription = description;
@@ -14,30 +29,113 @@ public class Task implements TaskInterface {
         endDate = end;
         theStatus = status;
     }
-    //add get methods
 
+    /**
+     * Gets the name of the task
+     *
+     * @return the name of the task
+     */
+    public String getName() {
+        return this.theName;
+    }
+
+    /**
+     * Changes the name of the task
+     *
+     * @param s is the name to change to
+     */
     public void changeName(String s) {
-
+        theName = s;
     }
 
+    /**
+     * Gets description of task
+     *
+     * @return description of task
+     */
+    public String getDescription() {
+        return this.theDescription;
+    }
+
+    /**
+     * Changes the description of task
+     *
+     * @param s is the description to change to
+     */
     public void changeDescription(String s) {
-
+        theDescription = s;
     }
 
+    /**
+     * Gets the priority of the task
+     *
+     * @return the priority type
+     */
+    public String getPriority() {
+        return this.thePriority;
+    }
+
+    /**
+     * Changes the priority of the task
+     *
+     * @param s is the what the priority changes to
+     */
     public void changePriority(String s) {
-
+        thePriority = s;
     }
 
+    /**
+     * Gets the start of the date
+     *
+     * @return the start of the date
+     */
+    public double getStart() {
+        return this.startDate;
+    }
+
+    /**
+     * Changes the start of the date
+     *
+     * @param d is what the start date changes to
+     */
     public void changeStart(double d) {
-
+        startDate = d;
     }
 
+    /**
+     * Gets the end date
+     *
+     * @return the end date
+     */
+    public double getEnd() {
+        return this.endDate;
+    }
+
+    /**
+     * Changes the end date
+     *
+     * @param d is what the end date changes to
+     */
     public void changeEnd(double d) {
-
+        endDate = d;
     }
 
-    public boolean changeStatus(boolean b) {
+    /**
+     * Gets the status of the task
+     *
+     * @return the status of the task
+     */
+    public boolean getStatus() {
+        return this.theStatus;
+    }
 
+    /**
+     * Changes the status of the task
+     *
+     * @param b is what the task changes to
+     */
+    public void changeStatus(boolean b) {
+        theStatus = b;
     }
 
 }
--- src/TaskInterface.java
+++ src/TaskInterface.java
@@ -1,15 +1,49 @@
+/**
+ * Author @Group 7
+ * Interface for all tasks
+ */
 public interface TaskInterface {
-    //sophia yuan
+
+    /**
+     * Changes name of task
+     *
+     * @param s is the name changed to
+     */
     void changeName(String s);
 
+    /**
+     * Changes description of task
+     *
+     * @param s is the description changed to
+     */
     void changeDescription(String s);
 
+    /**
+     * Changes priority of task
+     *
+     * @param s is the priority type changed to
+     */
     void changePriority(String s);
 
+    /**
+     * Changes start date of task
+     *
+     * @param d is the date changed to
+     */
     void changeStart(double d);
 
+    /**
+     * Changes end date of task
+     *
+     * @param d is the date changed to
+     */
     void changeEnd(double d);
 
-    boolean changeStatus(boolean b);
+    /**
+     * Changes status of task
+     *
+     * @param b is the status changed to
+     */
+    void changeStatus(boolean b);
 
 }
