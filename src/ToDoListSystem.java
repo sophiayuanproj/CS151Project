6d2310683634609deabbb91a7bd8b56d724cae1f
“Isla <isla.shi@sjsu.edu>
2020-11-09 12:20:50 -0800

    Isla: Added three more task button displays on the List panel. Started another panel for the Edit/Add button, it has an add/update button, a functioning back button, and most of the display buttons and text fields.

--- src/ToDoListSystem.java
+++ src/ToDoListSystem.java
@@ -9,26 +9,52 @@ public class ToDoListSystem extends JFrame {
     public static void main(String[] args) throws InterruptedException {
 
         JFrame frame = new JFrame("To-Do List");
-        Task task = new Task("a", "a", "a", 1, 2, true);
-        JButton taskButton = new JButton(task.toString());
+
+        //Sort, Rewards, and Add/Edit buttons
         JButton sortView = new JButton("Sort View");
         JButton rewards = new JButton("Rewards");
         JButton editTask = new JButton("Add/Edit");
-        JButton tasks = new JButton("Tasks");
 
-        //taskButton.setBackground(Color.WHITE);
-        taskButton.setBounds(50, 100, 700, 100);
+        //Set Bounds
         sortView.setBounds(550, 20, 100, 50);
         rewards.setBounds(650, 20, 100, 50);
         editTask.setBounds(750, 20, 100, 50);
+
+        //Tasks Title
+        JButton tasks = new JButton("Tasks");
         tasks.setBounds(50, 50, 150, 50);
         tasks.setFont(new Font("Arial", Font.PLAIN, 28));
 
-        frame.add(taskButton);
+        //Add all header buttons
         frame.add(sortView);
         frame.add(rewards);
         frame.add(editTask);
         frame.add(tasks);
+
+
+        //Task Buttons
+        Task task1 = new Task("Task 1", "a", "a", 1, 2, true);
+        JButton taskButton1 = new JButton(task1.toString());
+        taskButton1.setBounds(50, 120, 700, 100);
+
+        Task task2 = new Task("Task 2", "a", "a", 1, 2, true);
+        JButton taskButton2 = new JButton(task2.toString());
+        taskButton2.setBounds(50, 240, 700, 100);
+
+        Task task3 = new Task("Task 3", "a", "a", 1, 2, true);
+        JButton taskButton3 = new JButton(task3.toString());
+        taskButton3.setBounds(50, 360, 700, 100);
+
+        Task task4 = new Task("Task 4", "a", "a", 1, 2, true);
+        JButton taskButton4 = new JButton(task4.toString());
+        taskButton4.setBounds(50, 480, 700, 100);
+
+        frame.add(taskButton1);
+        frame.add(taskButton2);
+        frame.add(taskButton3);
+        frame.add(taskButton4);
+
+
 
         frame.setSize(900, 800);
         frame.getContentPane().setBackground(Color.pink);
@@ -42,7 +68,7 @@ public class ToDoListSystem extends JFrame {
 
                 JButton history = new JButton("History");
                 JButton back = new JButton("Back");
-                JButton reward = new JButton("Rwards");
+                JButton reward = new JButton("Rewards");
 
                 history.setBounds(550, 20, 100, 50);
                 back.setBounds(650, 20, 100, 50);
@@ -64,6 +90,83 @@ public class ToDoListSystem extends JFrame {
                         rewards.dispose();
                     }
                 });
+            }
+        });
+
+        editTask.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame editTask = new JFrame("Edit");
+
+                //Header buttons Add/Update and Back
+                JButton addUpdate = new JButton("Add/Update");
+                JButton back = new JButton("Back");
+                JButton title = new JButton("Task View");
+
+                addUpdate.setBounds(550, 20, 100, 50);
+                back.setBounds(650, 20, 100, 50);
+                title.setBounds(50, 50, 150, 50);
+                title.setFont(new Font("Arial", Font.PLAIN, 28));
+
+                editTask.add(addUpdate);
+                editTask.add(back);
+
+                //"Body" of the Panel
+                //side display buttons
+                JButton name = new JButton("Name");
+                JButton description = new JButton("Description");
+                JButton priority = new JButton("Priority");
+                JButton startDate = new JButton("Start Date");
+                JButton endDate = new JButton("End Date");
+
+                //set bounds of buttons
+                name.setBounds(50, 150, 100, 50);
+                description.setBounds(50, 220, 100,50);
+                priority.setBounds(50, 290, 100, 50);
+                startDate.setBounds(50, 360, 100, 50);
+                endDate.setBounds(50, 430, 100, 50);
+
+                editTask.add(name);
+                editTask.add(description);
+                editTask.add(priority);
+                editTask.add(startDate);
+                editTask.add(endDate);
+
+                //text fields
+                JTextField nameField = new JTextField();
+                JTextField descriptionField = new JTextField();
+                JTextField priorityField = new JTextField();
+                JTextField startDateField = new JTextField();
+                JTextField endDateField = new JTextField();
+
+                nameField.setBounds(200, 150, 500, 50);
+                descriptionField.setBounds(200, 220, 500,50);
+                priorityField.setBounds(200, 290, 500, 50);
+                startDateField.setBounds(200, 360, 500, 50);
+                endDateField.setBounds(200, 430, 500, 50);
+
+                editTask.add(nameField);
+                editTask.add(descriptionField);
+                editTask.add(priorityField);
+                editTask.add(startDateField);
+                editTask.add(endDateField);
+
+
+
+                editTask.setSize(900, 800);
+                editTask.getContentPane().setBackground(Color.pink);
+                editTask.setLayout(null);
+                editTask.setVisible(true);
+
+
+                //click on back, return to homepage
+                back.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        editTask.dispose();
+                    }
+                });
+
             }
         });
     }
