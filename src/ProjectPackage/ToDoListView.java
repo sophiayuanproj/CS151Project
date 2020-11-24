652ed5939856df4d8d0bb2267f07919782a46b57
DanleiQian <67854170+DanleiQian@users.noreply.github.com>
2020-11-23 22:37:13 -0800

    Split AddView from ToDoListView and add hint message of selecting only one option

--- src/ProjectPackage/ToDoListView.java
+++ src/ProjectPackage/ToDoListView.java
@@ -15,8 +15,7 @@ public class ToDoListView extends JFrame {
     /**
      * Counter for the space between each task's rectangle
      */
-    private int counter = 120;
-    public  JFrame frame;
+    public JFrame frame;
 
     public ToDoListView() {
 
@@ -26,13 +25,14 @@ public class ToDoListView extends JFrame {
 
     /**
      * To get the current list to be sorted
+     *
      * @return
      */
-    public ArrayList<Task> getCurrentList(){
+    public ArrayList<Task> getCurrentList() {
         return this.currentList;
     }
 
-    public void drawToDoList(){
+    public void drawToDoList() {
         frame = new JFrame("To-Do List");
 
         //Sort, Rewards, and Add buttons
@@ -78,13 +78,6 @@ public class ToDoListView extends JFrame {
         frame.add(taskButton3);
         frame.add(taskButton4);*/
 
-        /*
-        JPanel panel = new JPanel();
-        JScrollPane jsp = new JScrollPane(panel);
-        jsp.setVerticalScrollBarPolicy(jsp.VERTICAL_SCROLLBAR_ALWAYS);
-        frame.getContentPane().add(jsp, BorderLayout.EAST);
-        */
-
         frame.setSize(900, 800);
         //frame.getContentPane().setBackground(Color.lightGray);
         frame.setLayout(null);
@@ -93,215 +86,14 @@ public class ToDoListView extends JFrame {
         rewards.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-              RewardsView rv = new RewardsView(currentList);
+                RewardsView rv = new RewardsView(currentList);
             }
         });
 
         editTask.addActionListener(new ActionListener() {
-            JButton t = new JButton();
-
             @Override
             public void actionPerformed(ActionEvent e) {
-                JFrame editTask = new JFrame("Edit");
-
-                //Header buttons Add/Update and Back
-                JButton add = new JButton("Add");
-
-                JButton back = new JButton("Back");
-                JLabel title = new JLabel("Task View");
-
-                add.setBounds(550, 20, 100, 50);
-                back.setBounds(650, 20, 100, 50);
-                title.setBounds(50, 50, 150, 50);
-                title.setFont(new Font("Arial", Font.PLAIN, 28));
-
-                editTask.add(add);
-                editTask.add(back);
-
-                //"Body" of the Panel
-                //side display buttons
-                JButton name = new JButton("Name");
-                JButton description = new JButton("Description");
-                JButton priority = new JButton("Priority");
-                JButton startDate = new JButton("Start Date");
-                JButton endDate = new JButton("End Date");
-
-                //Can only be "Current" or "Finished"
-                JButton status = new JButton("Status");
-
-                //set bounds of buttons
-                name.setBounds(50, 150, 100, 50);
-                description.setBounds(50, 220, 100, 50);
-                priority.setBounds(50, 290, 100, 50);
-                startDate.setBounds(50, 360, 100, 50);
-                endDate.setBounds(50, 430, 100, 50);
-                status.setBounds(50, 500, 100, 50);
-
-                editTask.add(name);
-                editTask.add(description);
-                editTask.add(priority);
-                editTask.add(startDate);
-                editTask.add(endDate);
-                editTask.add(status);
-
-                //text fields
-                JCheckBox c1 = new JCheckBox("Normal Task");
-                JCheckBox c2 = new JCheckBox("Spending Plan");
-                JCheckBox c3 = new JCheckBox("Workout");
-                JTextField descriptionField = new JTextField();
-                JTextField priorityField = new JTextField();
-                JTextField startDateField = new JTextField();
-                JTextField endDateField = new JTextField();
-                JCheckBox c4 = new JCheckBox("Current");
-                JCheckBox c5 = new JCheckBox("Finished");
-
-                c1.setBounds(200, 150, 150, 50);
-                c2.setBounds(400,150,150,50);
-                c3.setBounds(600,150,150,50);
-                descriptionField.setBounds(200, 220, 500, 50);
-                priorityField.setBounds(200, 290, 500, 50);
-                startDateField.setBounds(200, 360, 500, 50);
-                endDateField.setBounds(200, 430, 500, 50);
-                c4.setBounds(200, 500, 150, 50);
-                c5.setBounds(400,500,150,50);
-
-                editTask.add(c1);
-                editTask.add(c2);
-                editTask.add(c3);
-                editTask.add(descriptionField);
-                editTask.add(priorityField);
-                editTask.add(startDateField);
-                editTask.add(endDateField);
-                editTask.add(c4);
-                editTask.add(c5);
-
-                editTask.setSize(900, 800);
-                //editTask.getContentPane().setBackground(Color.pink);
-                editTask.setLayout(null);
-                editTask.setVisible(true);
-
-                ArrayList<JCheckBox> typeBox = new ArrayList<JCheckBox>();
-                typeBox.add(c1);
-                typeBox.add(c2);
-                typeBox.add(c3);
-                ArrayList<JCheckBox> statusBox = new ArrayList<JCheckBox>();
-                statusBox.add(c4);
-                statusBox.add(c5);
-                //click on back, return to homepage
-                /**
-                 * Action for adding new task
-                 */
-                add.addActionListener(event ->
-                {
-                    System.out.println("0");
-                    double start;
-                    double end;
-                    String taskName = null;
-                    String taskStatus = null;
-                    try {
-                        start = Double.parseDouble(startDateField.getText());
-                        end = Double.parseDouble(endDateField.getText());
-
-                        for(JCheckBox j: typeBox) {
-                            if(j.isSelected()){
-                                taskName = j.getText();
-                            }
-                        }
-                        for(JCheckBox j: statusBox) {
-                            if(j.isSelected()){
-                                taskStatus = j.getText();
-                            }
-                        }
-
-                        Task temp = new Task(taskName, descriptionField.getText(),
-                                priorityField.getText(), start, end, taskStatus);
-
-                        t = new JButton(temp.toString());
-                        t.setBounds(50, counter, 700, 100);
-
-                        frame.add(t);
-                        currentList.add(temp);
-
-                        //testing
-                        System.out.println("This task is being added:" + temp.toString());
-                        System.out.println("This is the current list:" + currentList.get(0));
-
-                        t.addActionListener(new ActionListener() {
-                            @Override
-                            public void actionPerformed(ActionEvent e) {
-                                //System.out.println("Updated");
-                                editTask.setVisible(true);
-                                editTask.remove(add);
-                                JButton update = new JButton("Update");
-                                update.setBounds(550, 20, 100, 50);
-                                editTask.add(update);
-                                editTask.repaint();
-
-                                update.addActionListener(new ActionListener() {
-                                    @Override
-                                    public void actionPerformed(ActionEvent e) {
-                                        String taskName = null;
-                                        for(JCheckBox j: typeBox) {
-                                            if(j.isSelected()){
-                                                taskName = j.getText();
-                                            }
-                                        }
-                                        String taskStatus = null;
-                                        for(JCheckBox j: statusBox) {
-                                            if(j.isSelected()){
-                                                taskStatus = j.getText();
-                                            }
-                                        }
-                                        temp.changeName(taskName);
-                                        temp.changeDescription(descriptionField.getText());
-                                        temp.changePriority(priorityField.getText());
-                                        temp.changeStatus(taskStatus);
-
-                                        try {
-                                            temp.changeStart(Double.parseDouble(startDateField.getText()));
-                                            temp.changeEnd(Double.parseDouble(endDateField.getText()));
-                                        }catch (NumberFormatException numberFormatException) {
-                                            numberFormatException.printStackTrace();
-                                            JFrame errorMessage = new JFrame();
-                                            JLabel error = new JLabel("Please enter the date in the format mm.dd.", SwingConstants.CENTER);
-                                            errorMessage.add(error);
-                                            errorMessage.setSize(500, 200);
-                                            errorMessage.setVisible(true);
-                                        }
-                                        t.setText(temp.toString());
-                                        t.revalidate();
-                                    }
-                                });
-                            }
-                        });
-
-                        TaskManager tm = new TaskManager(currentList);
-                        //testing TaskManager
-                        tm.printArrayList();
-                        counter += 120;
-                        System.out.println("Created new task");
-
-                    } catch (NumberFormatException numberFormatException) {
-                        numberFormatException.printStackTrace();
-                        JFrame errorMessage = new JFrame();
-                        JLabel error = new JLabel("Please enter the date in the format mm.dd.", SwingConstants.CENTER);
-                        errorMessage.add(error);
-                        errorMessage.setSize(500, 200);
-                        errorMessage.setVisible(true);
-                    }
-                });
-                back.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        editTask.dispose();
-                    }
-                });
-
-                /**
-                 * Adds the new tasks
-                 * Everytime a new one is added, the counter int will increment
-                 * so the task rectangles won't overlap
-                 */
+                AddView ad = new AddView(frame, currentList);
             }
         });
 
@@ -315,16 +107,15 @@ public class ToDoListView extends JFrame {
 
     /**
      * Updating the view for sorting
+     *
      * @param t is the ArrayList to be passed in
      */
-    public void updateView(ArrayList<Task> t)
-    {
+    public void updateView(ArrayList<Task> t) {
         //testing
-        for(Task list : t) {
+        for (Task list : t) {
             System.out.println("This is ArrayList t: " + list);
         }
-        if(t.isEmpty())
-        {
+        if (t.isEmpty()) {
             System.out.println("list is empty");
         }
 
@@ -334,8 +125,7 @@ public class ToDoListView extends JFrame {
         updatedFrame.drawToDoList();
 
         int counter = 120;
-        for(Task task : t)
-        {
+        for (Task task : t) {
             JButton button = new JButton(task.toString());
             button.setBounds(50, counter, 700, 100);
             //System.out.println("THIS IS THE TASK TO UPDATE: " + task.toString());
